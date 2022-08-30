69ccb6d3310444857543bacc0e467df15a8f5083
RahulSatheeshS1 <108484080+RahulSatheeshS1@users.noreply.github.com>
2022-08-30 23:34:13 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
+import socket
+from unicodedata import name
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/rahul-details")
+@app.route("/details")
 def getDetails():
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
@@ -10,10 +12,10 @@ def getDetails():
         HostName=host_name,
         IpAddress=host_ip
     )
-@app.route("/api-health")
+@app.route("/health")
 def health():
     return jsonify(
         status="UP"
     )
 if __name__ == '__main__':
-      app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
